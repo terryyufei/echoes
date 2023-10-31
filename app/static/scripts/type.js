742ed028ac889b6836a1a2c4c05b7de8f05d8d5e
Terry Wambui <shikuwambui@gmail.com>
2023-11-01 02:25:37 +0300

    added a landing page

---
+++ app/static/scripts/type.js
@@ -0,0 +1,63 @@
+/**
+* Typewriter animation
+*/
+
+const TypeWriter = function (txtElement, words, wait = 3000) /* Create typewriter class */
+{
+      this.txtElement = txtElement;
+      this.words = words;
+      this.txt = '';
+      this.wordIndex = 0;
+      this.wait = parseInt(wait, 10);
+      this.type();
+      this.isDeleting = false;
+}
+
+TypeWriter.prototype.type = function() /* Type method */
+{
+
+      const current = this.wordIndex % this.words.length;
+      const fullTxt = this.words[current];
+  
+      if (this.isDeleting)
+      {
+          this.txt = fullTxt.substring(0, this.txt.length - 1);
+      }
+      else
+      {
+          this.txt = fullTxt.substring(0, this.txt.length + 1);
+      }
+  
+      this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+      let typeSpeed = 30; /** Reduce this to increase speed  */
+      
+      if (this.isDeleting)
+      {
+          typeSpeed /= 2;
+      }
+      
+      if (!this.isDeleting && this.txt === fullTxt)
+      {
+          typeSpeed = this.wait;
+          this.isDeleting = true;
+      }
+      else if (this.isDeleting && this.txt === '')
+      {
+          this.isDeleting = false;
+          this.wordIndex++;
+          typeSpeed = 500;
+      }
+
+      setTimeout(() => this.type(), typeSpeed);
+  }
+
+  document.addEventListener('DOMContentLoaded', init);
+
+  function init()
+  {
+      const txtElement = document.querySelector('.txt-type');
+      const words = JSON.parse(txtElement.getAttribute('data-words'));
+      const wait = txtElement.getAttribute('data-wait');
+      new TypeWriter(txtElement, words, wait);
+  }
